ff0d4d9f1ec589a2dc07fc99d68e01f5f03f55b2
Sérgio Damasceno de Matos <sergio.damasceno.matos@outlook.com>
2022-12-25 16:27:54 -0300

    Created PersonProfile.

---
+++ src/School.Application/Profiles/PersonProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using School.Application.DTO;
+using School.Domain.Entities;
+
+namespace School.Application.Profiles
+{
+    public class PersonProfile : Profile
+    {
+        public PersonProfile()
+        {
+            CreateMap<Person, PersonDto>()
+                .ForMember(dest => dest.Addresses, opt => opt.ExplicitExpansion())
+                .ForMember(dest => dest.Emails, opt => opt.ExplicitExpansion())
+                .ForMember(dest => dest.Phones, opt => opt.ExplicitExpansion());
+        }            
+    }
+}
