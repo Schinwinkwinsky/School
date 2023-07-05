5590d945c738519a46ff9a30e5d31a74ee3e8993
Sérgio Damasceno de Matos <sergio.damasceno.matos@outlook.com>
2023-07-05 12:56:18 -0300

    Updated pagination parameters.

--- src/School.WebAPI/Attributes/EnableQueryPaginatedResultAttribute.cs
+++ src/School.WebAPI/Attributes/EnableQueryPaginatedResultAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.OData.Extensions;
 using Microsoft.AspNetCore.OData.Query;
 using School.Application.Results;
 
@@ -9,24 +10,26 @@ namespace School.WebAPI.Attributes
     {
         private ODataQueryOptions? _oDataQueryOptions = null;
 
-        public override IQueryable ApplyQuery(IQueryable queryable, ODataQueryOptions queryOptions)
+        public override void ValidateQuery(HttpRequest request, ODataQueryOptions queryOptions)
         {
             _oDataQueryOptions = queryOptions;
 
-            return base.ApplyQuery(queryable, queryOptions);
+            base.ValidateQuery(request, queryOptions);
         }
 
         public override void OnActionExecuted(ActionExecutedContext actionExecutedContext)
         {
-            if (actionExecutedContext.Exception != null) return;
+            if (actionExecutedContext.Exception != null) return;            
 
             base.OnActionExecuted(actionExecutedContext);
 
+            var odataFeatures = actionExecutedContext.HttpContext.Request.ODataFeature();
+            var count = odataFeatures.TotalCount;
             var skip = _oDataQueryOptions?.Skip?.Value;
             var top = _oDataQueryOptions?.Top?.Value;
 
             if (actionExecutedContext.Result is ObjectResult obj && obj.Value is IQueryable<object> queryable)
-                actionExecutedContext.Result = new ObjectResult(PaginatedResult<object>.Success(queryable, queryable.Count(), skip, top));
+                actionExecutedContext.Result = new ObjectResult(PaginatedResult<object>.Success(queryable, count, skip, top));
         }
     }
 }
