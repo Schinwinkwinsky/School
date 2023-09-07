bccf31e222f551368731de44aab6556b1ef0a1db
Sérgio Damasceno de Matos <sergio.damasceno.matos@outlook.com>
2023-09-07 09:05:25 -0300

    Excluded WebAPI.Program.cs from tests.

--- src/School.Application/Helpers/StringExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace School.Application.Helpers;
-
-public static class StringExtensions
-{
-    public static string Capitalize(this string value)
-    {
-        return value[..1].ToUpper() + value[1..];
-    }
-}
